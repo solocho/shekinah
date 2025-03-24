4e285c1667065baa2fbbf1d767aba287c18c7989
solocho <solocho264@gmail.com>
2025-03-24 23:45:51 +0300

    Update about.js

--- about.js
+++ about.js
@@ -1,4 +1,13 @@
 function toggleMenu() {
-    const nav = document.querySelector('.nav-links');
-    nav.classList.toggle('active');
+    let menu = document.querySelector(".nav-links");
+    let menuToggle = document.querySelector(".menu-toggle");
+
+    menu.classList.toggle("active");
+
+    // Change the menu icon between ☰ and ✖ when clicked
+    if (menu.classList.contains("active")) {
+        menuToggle.innerHTML = "✖"; // Close icon
+    } else {
+        menuToggle.innerHTML = "☰"; // Hamburger icon
+    }
 }
