c5f4490f4f866d099143a4734257187918a61370
solocho <solocho264@gmail.com>
2025-03-24 23:43:47 +0300

    Update about.js

--- about.js
+++ about.js
@@ -1,10 +1,4 @@
 function toggleMenu() {
-    let menu = document.querySelector(".nav-links");
-    let isVisible = menu.classList.contains("active");
-
-    if (isVisible) {
-        menu.classList.remove("active");
-    } else {
-        menu.classList.add("active");
-    }
+    const nav = document.querySelector('.nav-links');
+    nav.classList.toggle('active');
 }
